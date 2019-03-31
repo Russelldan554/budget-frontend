00b26225e742a3fa11beb11ca82e3ca5262f1d3a
daniel <daniel@kasmweb.com>
2019-03-31 19:29:18 -0400

    Adding a simple Design for the Accounts page.

--- src/Views/_Accounts.jsx
+++ src/Views/_Accounts.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {} from "reactstrap";
+import {Container, Row, Col, Card, CardHeader, CardBody, CardFooter } from 'reactstrap';
 
 class Accounts extends Component{
   constructor(props) {
@@ -9,7 +9,50 @@ class Accounts extends Component{
   render() {
     return (
       <div>
-          Accounts
+          <Row>
+            <Col>
+              <Card xs={11}>
+                <CardHeader>
+                  Accounts
+                </CardHeader>
+                <CardBody>
+                  <Row>
+                    {/*we will change this to a for loop for how ever many accounts are available */}
+                    <Col sm={4} align="center">
+                      <Card>
+                        Account One <br />
+                        Bank Name
+                        <hr />
+                        Type: Checking <br />
+                        Total: $5000.00
+                      </Card>
+                    </Col>
+                    <Col sm={4} align="center">
+                      <Card>
+                        Account Two <br />
+                        Bank Name
+                        <hr />
+                        Type: Savings <br />
+                      Total: $10000.00
+                      </Card>
+                    </Col>
+                    <Col sm={4} align="center">
+                      <Card>
+                        Account Three <br />
+                        Bank Name
+                        <hr />
+                        Type: Credit <br />
+                      Total: $250.00
+                      </Card>
+                    </Col>
+                  </Row>
+                </CardBody>
+                <CardFooter>
+                  Total: $14750.00
+                </CardFooter>
+              </Card>
+            </Col>
+          </Row>
       </div>
     );
   }
