1cb28a34340583cdcb492daafd3afcef1527576c
William Crutchfield <william.r.crutchfield@gmail.com>
2019-04-29 04:13:28 -0700

    Footer

--- src/Components/Footer/_Footer.jsx
+++ src/Components/Footer/_Footer.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import {Container, Row, Col} from 'reactstrap';
+import {
+  Col, Container, Row,
+} from 'reactstrap';
 
-const Footer  = props => {
-  return (
-    <Container className="footer bg-dark" fluid>
-      <Row noGutters>
-        <Col className="text-left" xs={11}>
+const Footer = () => (
+  <Container className="footer bg-dark" fluid>
+    <Row noGutters>
+      <Col className="text-left" xs={11}>
         <div>
-          This project was developed for CMSC 495 at UMUC
+          {'This project was developed for CMSC 495 at UMUC'}
         </div>
-        </Col>
-        <Col className="text-right my-auto" xs={1}>
-          <a href="https://github.com/Russelldan554/budget-app">
-            <i className="fa fa-github"></i>
-          </a>
-        </Col>
-      </Row>
-    </Container>
-  );
-}
+      </Col>
+      <Col className="text-right my-auto" xs={1}>
+        <a href="https://github.com/Russelldan554/budget-app">
+          <i className="fa fa-github" />
+        </a>
+      </Col>
+    </Row>
+  </Container>
+);
 
 export default Footer;
