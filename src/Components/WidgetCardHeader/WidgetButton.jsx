4d333f3b69038e1c0a5c32adf5fe2fb5298ef8a1
William Crutchfield <william.r.crutchfield@gmail.com>
2019-04-30 00:06:13 -0700

    WidgetCardHeader

--- src/Components/WidgetCardHeader/WidgetButton.jsx
+++ src/Components/WidgetCardHeader/WidgetButton.jsx
@@ -4,58 +4,75 @@ import PropTypes from 'prop-types';
 
 class WidgetButton extends Component {
   constructor(props) {
-      super(props);
-      this.toggleToolTip = this.toggleToolTip.bind(this);
-      this.state = {
-        tooltipOpen: false,
-        color: "",
-        icon: ""
+    super(props);
+    this.toggleToolTip = this.toggleToolTip.bind(this);
+    this.state = {
+      tooltipOpen: false,
+      color: '',
+      icon: '',
+    };
+  }
+
+  componentDidMount() {
+    if (this.props.buttonType === 'add') {
+      this.setState({
+        color: 'success',
+        icon: 'fa fa-plus',
+      });
+    } else if (this.props.buttonType === 'remove') {
+      this.setState({
+        color: 'danger',
+        icon: 'fa fa-minus',
+      });
     }
   }
 
   toggleToolTip() {
+    const {
+      tooltipOpen,
+    } = this.state;
+
     this.setState({
-      tooltipOpen: !this.state.tooltipOpen
+      tooltipOpen: !tooltipOpen,
     });
   }
 
-  componentDidMount() {
-      if (this.props.buttonType === "add") {
-        this.setState ({
-          color: "success",
-          icon: "fa fa-plus"
-        })
-      } else if (this.props.buttonType === "remove") {
-        this.setState ({
-            color: "danger",
-            icon: "fa fa-minus"
-        })
-      }
-  }
-
   render() {
+    const {
+      buttonHandler,
+      id,
+      tooltipMessage,
+    } = this.props;
+
+    const {
+      color,
+      icon,
+      tooltipOpen,
+    } = this.state;
+
+
     return (
       <div className="createButton d-inline-block">
         <div className="d-flex justify-content-end">
-          <Button 
-            className="align-self-center" 
-            color={this.state.color} 
-            id={'tooltip-' + this.props.id} 
-            onClick={this.props.buttonHandler}
-            outline 
+          <Button
+            className="align-self-center"
+            color={color}
+            id={`tooltip-${id}`}
+            onClick={buttonHandler}
+            outline
           >
-            <i className={this.state.icon} aria-hidden="true"></i>
+            <i className={icon} aria-hidden="true" />
           </Button>
-          <Tooltip 
-            placement="top" 
-            isOpen={this.state.tooltipOpen} 
-            target={'tooltip-' + this.props.id} 
-            toggle={this.toggleToolTip} 
-            autohide={true} 
+          <Tooltip
+            placement="top"
+            isOpen={tooltipOpen}
+            target={`tooltip-${id}`}
+            toggle={this.toggleToolTip}
+            autohide
             trigger="hover focus"
             delay={{ show: 0, hide: 100 }}
           >
-            {this.props.tooltipMessage}
+            {tooltipMessage}
           </Tooltip>
         </div>
       </div>
@@ -68,6 +85,10 @@ WidgetButton.propTypes = {
   buttonType: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
   tooltipMessage: PropTypes.string.isRequired,
+};
+
+WidgetButton.defaultProps = {
+  buttonHandler: () => {},
 };
 
 export default WidgetButton;
--- src/Components/WidgetCardHeader/_WidgetCardHeader.jsx
+++ src/Components/WidgetCardHeader/_WidgetCardHeader.jsx
@@ -2,54 +2,73 @@ import React, { Component } from 'react';
 import { CardHeader, Col, Row } from 'reactstrap';
 import PropTypes from 'prop-types';
 
-import WidgetButton from './WidgetButton'
+import WidgetButton from './WidgetButton';
 import NewEntityModal from './NewEntityModal';
 
 class WidgetCardHeader extends Component {
   constructor(props) {
     super(props);
-    this.buttonType = ["remove", "add"]
+    this.buttonType = ['remove', 'add'];
     this.toggleCreateModal = this.toggleCreateModal.bind(this);
 
     this.state = {
-      createModal: false
-    }
+      createModal: false,
+    };
   }
 
   toggleCreateModal() {
-    this.setState ({
-      createModal: !this.state.createModal
-    })
+    const {
+      createModal,
+    } = this.state;
+
+    this.setState({
+      createModal: !createModal,
+    });
   }
 
   render() {
+    const {
+      actionTitle,
+      actionConfirm,
+      buttonID,
+      cardHeader,
+      getAccounts,
+      getBudgets,
+      toggleDeleteButtons,
+    } = this.props;
+
+    const {
+      createModal,
+    } = this.state;
+
     return (
       <CardHeader>
         <Row className="info" noGutters>
           <Col xs={8}>
-            <h3 className="cardHeader">{this.props.cardHeader}</h3>
+            <h3 className="cardHeader">{cardHeader}</h3>
           </Col>
           <Col xs={4} className="d-flex justify-content-end">
-            <WidgetButton 
-              id={this.buttonType[0] + this.props.buttonID} 
-              tooltipMessage={"Remove a" + this.props.actionTitle} 
-              buttonHandler={this.props.toggleDeleteButtons} 
-              buttonType={this.buttonType[0]} 
+            <WidgetButton
+              id={this.buttonType[0] + buttonID}
+              tooltipMessage={`Remove a ${actionTitle}`}
+              buttonHandler={toggleDeleteButtons}
+              buttonType={this.buttonType[0]}
             />
-            <WidgetButton 
-              id={this.buttonType[1] + this.props.buttonID} 
-              tooltipMessage={"Add a" + this.props.actionTitle} 
-              buttonHandler={this.toggleCreateModal} 
-              buttonType={this.buttonType[1]} 
+            <WidgetButton
+              id={this.buttonType[1] + buttonID}
+              tooltipMessage={`Add a ${actionTitle}`}
+              buttonHandler={this.toggleCreateModal}
+              buttonType={this.buttonType[1]}
             />
-            <NewEntityModal 
-              modalTitle={this.props.actionTitle} 
-              actionConfirm={this.props.actionConfirm} 
-              modalBody={this.props.cardHeader} 
-              getAccounts={this.props.getAccounts}
-              getBudgets={this.props.getBudgets} 
-              buttonHandler={this.toggleCreateModal} 
-              buttonState={this.state.createModal} />
+            <NewEntityModal
+              modalTitle={actionTitle}
+              actionConfirm={actionConfirm}
+              modalBody={cardHeader}
+              getAccounts={getAccounts}
+              getBudgets={getBudgets}
+              buttonHandler={this.toggleCreateModal}
+              buttonState={createModal}
+            />
           </Col>
         </Row>
       </CardHeader>
@@ -64,7 +83,12 @@ WidgetCardHeader.propTypes = {
   cardHeader: PropTypes.string.isRequired,
   getAccounts: PropTypes.func,
   getBudgets: PropTypes.func,
-  toggleDeleteButtons: PropTypes.func
+  toggleDeleteButtons: PropTypes.func.isRequired,
+};
+
+WidgetCardHeader.defaultProps = {
+  getAccounts: () => {},
+  getBudgets: () => {},
 };
 
 export default WidgetCardHeader;
