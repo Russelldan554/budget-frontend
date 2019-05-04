d96bbcc9bd73ce3783a38c5e02d3bb7a31f5a985
William Crutchfield <william.r.crutchfield@gmail.com>
2019-05-04 03:45:19 -0700

    Adds Remaining API Calls

--- src/Components/API.js
+++ src/Components/API.js
@@ -7,72 +7,11 @@ const headers = {
   'Content-Type': 'application/json',
 };
 
-export const getUsers = (payload => axios(
-  `${URL}/users`, {
-    method: 'GET',
-    headers,
-    data: payload,
-  },
-)
-  .then(response => response.data)
-  .catch((error) => {
-    throw error;
-  })
-);
+// Users Endpoint
+//
+//
 
-export const getUser = (payload => axios(
-  `${URL}/users/${userID}`, {
-    method: 'GET',
-    headers,
-    data: payload,
-  },
-)
-  .then(response => response.data)
-  .catch((error) => {
-    throw error;
-  })
-);
-
-export const getAccounts = (payload => axios(
-  `${URL}/users/${userID}/accounts`, {
-    method: 'GET',
-    headers,
-    data: payload,
-  },
-)
-  .then(response => response.data)
-  .catch((error) => {
-    throw error;
-  })
-);
-
-export const getTransactions = (payload => axios(
-  `${URL}/users/${userID}/transactions`, {
-    method: 'GET',
-    headers,
-    data: payload,
-  },
-)
-  .then(response => response.data)
-  .catch((error) => {
-    throw error;
-  })
-);
-
-export const getBudgets = (payload => axios(
-  `${URL}/users/${userID}/budgets`, {
-    method: 'GET',
-    headers,
-    data: payload,
-  },
-)
-  .then(response => response.data)
-  .catch((error) => {
-    throw error;
-  })
-);
-
-// This is experimental call for login not implemented on backend yet
+// Login - This is experimental call for login not implemented on backend yet
 export const login = ((userName, password) => axios(
   `${URL}/users/login`, {
     method: 'POST',
@@ -91,6 +30,35 @@ export const login = ((userName, password) => axios(
   })
 );
 
+// Get All Users
+export const getUsers = (payload => axios(
+  `${URL}/users`, {
+    method: 'GET',
+    headers,
+    data: payload,
+  },
+)
+  .then(response => response.data)
+  .catch((error) => {
+    throw error;
+  })
+);
+
+// Get User
+export const getUser = (payload => axios(
+  `${URL}/users/${userID}`, {
+    method: 'GET',
+    headers,
+    data: payload,
+  },
+)
+  .then(response => response.data)
+  .catch((error) => {
+    throw error;
+  })
+);
+
+// Add User
 export const addUser = (payload => axios(
   `${URL}/users`, {
     method: 'POST',
@@ -104,6 +72,93 @@ export const addUser = (payload => axios(
   })
 );
 
+// Update User
+export const updateUser = (payload => axios(
+  `${URL}/users/${userID}`, {
+    method: 'PUT',
+    headers,
+    data: payload,
+  },
+)
+  .then(response => response.data)
+  .catch((error) => {
+    throw error;
+  })
+);
+
+// Delete User
+export const deleteUser = (() => axios(
+  `${URL}/users/${userID}`, {
+    method: 'DELETE',
+    headers,
+  },
+)
+  .then(response => response.data)
+  .catch((error) => {
+    throw error;
+  })
+);
+
+// Update Specific User Fields
+export const updateUserFields = (payload => axios(
+  `${URL}/users/${userID}/update`, {
+    method: 'PUT',
+    headers,
+    data: payload,
+  },
+)
+  .then(response => response.data)
+  .catch((error) => {
+    throw error;
+  })
+);
+
+// Accounts
+//
+//
+
+// Get All Accounts of User
+export const getAccounts = (payload => axios(
+  `${URL}/users/${userID}/accounts`, {
+    method: 'GET',
+    headers,
+    data: payload,
+  },
+)
+  .then(response => response.data)
+  .catch((error) => {
+    throw error;
+  })
+);
+
+// Get Account
+export const getAccount = (payload => axios(
+  `${URL}/users/${userID}/accounts/${payload.accountID}`, {
+    method: 'GET',
+    headers,
+    data: payload,
+  },
+)
+  .then(response => response.data)
+  .catch((error) => {
+    throw error;
+  })
+);
+
+// Delete Account
+export const deleteAccount = ((userId, id) => axios(
+  `${URL}/users/${userId}/accounts/${id}`, {
+    method: 'DELETE',
+    headers,
+  },
+)
+  .then(response => response.data)
+  .catch((error) => {
+    throw error;
+  })
+);
+
+// Add Account
 export const addAccount = ((id, payload) => axios(
   `${URL}/users/${userID}/accounts`, {
     method: 'POST',
@@ -117,7 +172,68 @@ export const addAccount = ((id, payload) => axios(
   })
 );
 
+// Update Account
+export const updateAccount = (payload => axios(
+  `${URL}/users/${userID}/accounts/${payload.accountID}`, {
+    method: 'PUT',
+    headers,
+    data: payload,
+  },
+)
+  .then(response => response.data)
+  .catch((error) => {
+    throw error;
+  })
+);
 
+// Transactions
+//
+//
+
+// Get All Transactions for User
+export const getTransactions = (payload => axios(
+  `${URL}/users/${userID}/transactions`, {
+    method: 'GET',
+    headers,
+    data: payload,
+  },
+)
+  .then(response => response.data)
+  .catch((error) => {
+    throw error;
+  })
+);
+
+// Get All Transaction for Account
+export const getAccountTransactions = (payload => axios(
+  `${URL}/users/${userID}/accounts/${payload.accountID}/transactions`, {
+    method: 'GET',
+    headers,
+    data: payload,
+  },
+)
+  .then(response => response.data)
+  .catch((error) => {
+    throw error;
+  })
+);
+
+// Get Transaction
+export const getTransaction = (payload => axios(
+  `${URL}/users/${userID}/accounts/${payload.accountID}/transactions/
+  ${payload.transactionID}`, {
+    method: 'GET',
+    headers,
+    data: payload,
+  },
+)
+  .then(response => response.data)
+  .catch((error) => {
+    throw error;
+  })
+);
+
+// Add Transaction
 export const addTransaction = (payload => axios(
   `${URL}/users/${userID}/accounts/${payload.accountID}/transactions`, {
     method: 'POST',
@@ -137,6 +253,68 @@ export const addTransaction = (payload => axios(
   })
 );
 
+// Update Transaction
+export const updateTransaction = (payload => axios(
+  `${URL}/users/${userID}/accounts/${payload.accountID}/transactions/
+  ${payload.transactionID}`, {
+    method: 'PUT',
+    headers,
+    data: payload,
+  },
+)
+  .then(response => response.data)
+  .catch((error) => {
+    throw error;
+  })
+);
+
+// Delete Transaction
+export const deleteTransaction = (payload => axios(
+  `${URL}/users/${userID}/accounts/${payload.accountID}/transactions/
+  ${payload.transactionID}`, {
+    method: 'DELETE',
+    headers,
+  },
+)
+  .then(response => response.data)
+  .catch((error) => {
+    throw error;
+  })
+);
+
+// Budgets
+//
+//
+
+// Get All Budgets
+export const getBudgets = (payload => axios(
+  `${URL}/users/${userID}/budgets`, {
+    method: 'GET',
+    headers,
+    data: payload,
+  },
+)
+  .then(response => response.data)
+  .catch((error) => {
+    throw error;
+  })
+);
+
+// Get Budget
+export const getBudget = (payload => axios(
+  `${URL}/users/${userID}/budgets/${payload.budgetID}`, {
+    method: 'GET',
+    headers,
+    data: payload,
+  },
+)
+  .then(response => response.data)
+  .catch((error) => {
+    throw error;
+  })
+);
+
+// Add Budget
 export const addBudget = ((id, payload) => axios(
   `${URL}/users/${userID}/budgets`, {
     method: 'POST',
@@ -150,8 +328,9 @@ export const addBudget = ((id, payload) => axios(
   })
 );
 
-export const updateUser = (payload => axios(
-  `${URL}/users/${userID}`, {
+// Update Budget
+export const updateBudget = (payload => axios(
+  `${URL}/users/${userID}/budgets/${payload.budgetID}`, {
     method: 'PUT',
     headers,
     data: payload,
@@ -163,18 +342,7 @@ export const updateUser = (payload => axios(
   })
 );
 
-export const deleteAccount = ((userId, id) => axios(
-  `${URL}/users/${userId}/accounts/${id}`, {
-    method: 'DELETE',
-    headers,
-  },
-)
-  .then(response => response.data)
-  .catch((error) => {
-    throw error;
-  })
-);
-
+// Delete Budget
 export const deleteBudget = ((userId, id) => axios(
   `${URL}/users/${userId}/budgets/${id}`, {
     method: 'DELETE',
