aaa8f03fcbc1219fbd68079fc5e5203f1958f748
Daniel Russell <Russelldan554@gmail.com>
2019-05-05 12:07:08 -0400

    Update API.js

--- src/Components/API.js
+++ src/Components/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // Comment out whichever one you need for testing
-// const URL = 'https://moolah-backend.herokuapp.com/';
-const URL = 'http://localhost:8080';
+const URL = 'https://moolah-backend.herokuapp.com/';
+//const URL = 'http://localhost:8080';
 const userID = localStorage.getItem('userId');
 const headers = {
   'Content-Type': 'application/json',
